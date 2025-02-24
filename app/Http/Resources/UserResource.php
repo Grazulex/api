cf7115660b35a21fc8925755167f148ea4dc7b66
Jean-Marc Strauven <jms@grazulex.be>
2025-02-24 08:51:55 +0100

    add resrouce

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @property User $resource */
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+            'email' => [
+                'address' => $this->resource->email,
+                'verified' => $this->resource->hasVerifiedEmail(),
+
+            ],
+            'created' => new DateResource($this->resource->created_at),
+            'workspace' =>  new WorkspaceResource($this->whenLoaded('workspace'))
+        ];
+    }
+}
--- routes/api/routes.php
+++ routes/api/routes.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
@@ -15,7 +16,7 @@ Route::tenanted(function (Router $router) {
 
     $router->middleware(['auth:sanctum'])->group(static function () use ($router): void {
         $router->get('/users', function () {
-            return response()->json(User::all());
+            return response()->json(UserResource::collection(User::query()->simplePaginate()));
         });
     });
 });
